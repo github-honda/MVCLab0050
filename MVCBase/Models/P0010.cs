081503b097f1a503d829c9d06730206cf2fae019
github-honda <honda@luckstar.com.tw>
2016-10-06 17:16:51 +0800

    First commit

---
+++ MVCBase/Models/P0010.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBase.Models
+{
+    public class P0010
+    {
+        public string ms1 { get; set; }
+        public string ms2 { get; set; }
+        public int mi1 { get; set; }
+        public int mi2 { get; set; }
+    }
+}
---
+++ MVCBase/ViewModels/P0010ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBase.ViewModels
+{
+    public class P0010ViewModel
+    {
+        public string ms1 { get; set; }
+        public string ms2 { get; set; }
+        public int mi1 { get; set; }
+        public int mi2 { get; set; }
+
+        public int miSum { get; set; }
+        public int mi1Extra { get; set; }
+        public string msColor { get; set; }
+    }
+}
